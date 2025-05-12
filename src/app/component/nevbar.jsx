7b010d8decfe2196d667976e95d60b4b4d5207e8
parewaa <eisariya.pw@gmail.com>
2025-05-12 19:59:32 +0700

    navbar,resgister

--- src/app/component/nevbar.jsx
+++ src/app/component/nevbar.jsx
@@ -2,24 +2,33 @@ import React from 'react'
 import { BiHome } from "react-icons/bi";
 import { IoMdSearch } from "react-icons/io";
 import { FaUser } from "react-icons/fa";
+import { MdBakeryDining } from "react-icons/md";
 
 function nevbar() {
   return (
-    <div className="navbar bg-base-100 shadow-sm">
-    <div className="navbar-start">
-        <img src= "/logo-full.png" alt="Logo" className="w-20" />
-        <button className="btn btn-ghost"><BiHome />Home</button>
-    </div>
+    <div className="navbar bg-base-100 shadow-sm rounded-xl">
+        <div className=' navbar p-2'>
+            <div className="navbar-start">
+                <img src= "/logo.png" alt="Logo" className="w-10" />
+                <ul className='menu menu-horizontal'>
+                    <li className='pl-3'> <button className="btn btn-ghost"><MdBakeryDining className='h-8 w-8'/>Home</button> </li>
+                </ul>
+            </div>
 
-    <div className="navbar-end">
-        <button className="btn">
-            <IoMdSearch type="search" className="grow" placeholder="Search"/>
-        <input type="search" className="grow" placeholder="Search" />
-        </button>
-        <button className="btn btn-ghost btn-circle">
-            <FaUser />
-        </button>
-    </div>
+            <div className="navbar-end gap-4">
+                <label className="input input-bordered flex items-center w-60">
+                        <IoMdSearch className="text-gray-400" />
+                        <input
+                        type="search"
+                        className="grow"
+                        placeholder="search"
+                        />
+                </label>
+                <button className="btn btn-ghost btn-circle">
+                    <FaUser />
+                </button>
+            </div>
+        </div>
     </div>
   )
 }
--- src/app/front_office/register/page.jsx
+++ src/app/front_office/register/page.jsx
@@ -2,66 +2,71 @@
 import { useState } from 'react'
 import { FaUser, FaLock } from 'react-icons/fa'
 import { useRouter } from 'next/navigation'
+import { MdEmail } from "react-icons/md";
+import Nevbar from '@/app/component/nevbar'
 import Link from 'next/link'
 
 export default function LoginPage() {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const router = useRouter()
+  // const [username, setUsername] = useState('')
+  // const [password, setPassword] = useState('')
+  // const router = useRouter()
   
-  const handleLogin = async (e) => {
-    e.preventDefault()
+  // const handleLogin = async (e) => {
+  //   e.preventDefault()
   
-    const res = await fetch('/api/login', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ username, password }),
-    })
+  //   const res = await fetch('/api/login', {
+  //     method: 'POST',
+  //     headers: { 'Content-Type': 'application/json' },
+  //     body: JSON.stringify({ username, password }),
+  //   })
   
-    if (!res.ok) {
-      alert('Login ไม่สำเร็จ')
-      return
-    }
+  //   if (!res.ok) {
+  //     alert('Login ไม่สำเร็จ')
+  //     return
+  //   }
   
-    const data = await res.json()
+  //   const data = await res.json()
   
-    if (data.success) {
-      alert('Login สำเร็จ')
-      router.push('/back_office/order')
-    } else {
-      alert('Login ไม่สำเร็จ')
-    }
-  }
+  //   if (data.success) {
+  //     alert('Login สำเร็จ')
+  //     router.push('/back_office/order')
+  //   } else {
+  //     alert('Login ไม่สำเร็จ')
+  //   }
+  // }
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-white ">
-      <div className="w-full max-w-sm p-8 rounded-lg bg-white">
-        {/* Logo */}
-        <div className="flex flex-col items-center mb-6">
-          <img src="/logo-full.png" alt="Logo" className="w-40 h-18 mb-2" />
-        </div>
-
-        {/* Login Form */}
-          {/* change button */}
+    <div className='flex flex-col p-4'>
+      <Nevbar />    
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="w-full max-w-sm p-8 rounded-lg">
+        {/* Register Form */}
           <div className="flex justify-center join join-vertical lg:join-horizontal">
             <Link href={"/front_office/login"}>
-            <button className='btn btn-secondary join-item'>Login</button>
+            <button className="btn text-white join-item" style={{ backgroundColor: '#ED9A82' }}>Login</button>
             </Link>
             <Link href={"/front_office/register"}>
-            <button className='btn btn-secondary join-item'>Register</button>
+            <button className="btn text-white join-item" style={{ backgroundColor: '#B12937' }}>Register</button>
             </Link>
           </div>
-        <h2 className="text-center text-xl font-semibold mb-4">Login</h2>
-        <form onSubmit={handleLogin} className="space-y-4">
+        <h2 className="text-center text-xl font-semibold mb-4">register</h2>
+        {/* <form onSubmit={handleLogin} className="space-y-4"> */}
           <label className="input input-bordered flex items-center gap-2 mt-5">
             <FaUser className="text-gray-400" />
             <input
               type="text"
               className="grow"
-              placeholder="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              required
+              placeholder="full-name"
+              // required
+            />
+          </label>
+          <label className="input input-bordered flex items-center gap-2 mt-5">
+            <MdEmail className="text-gray-400" />
+            <input
+              type="password"
+              className="grow"
+              placeholder="email"
+              // required
             />
           </label>
           <label className="input input-bordered flex items-center gap-2 mt-5">
@@ -70,18 +75,28 @@ export default function LoginPage() {
               type="password"
               className="grow"
               placeholder="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
+              // value={password}
+              // onChange={(e) => setPassword(e.target.value)}
+              // required
+            />
+          </label>
+          <label className="input input-bordered flex items-center gap-2 mt-5">
+            <FaLock className="text-gray-400" />
+            <input
+              type="password"
+              className="grow"
+              placeholder="re-password"
+              // value={password}
+              // onChange={(e) => setPassword(e.target.value)}
+              // required
             />
           </label>
           <div className="flex flex-col items-center mt-6">
-            <button type="submit" className="btn btn-lg  btn-outline btn-error w-40">
-              Login
-            </button>
+            <button className="btn btn-outline btn-error btn-lg">register</button>
           </div>
-        </form>
+        {/* </form> */}
       </div>
     </div>
+  </div>
   )
 }
