af72d61deaaa7b60815b93154b88b28c2cb52148
parewaa <eisariya.pw@gmail.com>
2025-05-12 18:10:37 +0700

    nevbar

--- src/app/component/nevbar.jsx
+++ src/app/component/nevbar.jsx
@@ -1,34 +1,26 @@
 import React from 'react'
+import { BiHome } from "react-icons/bi";
+import { IoMdSearch } from "react-icons/io";
+import { FaUser } from "react-icons/fa";
 
 function nevbar() {
   return (
     <div className="navbar bg-base-100 shadow-sm">
     <div className="navbar-start">
-        <div className="dropdown">
-        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
-        </div>
-        <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-            <li><a>Home</a></li>
-            <li><a>Menu</a></li>
-            <li><a></a></li>
-        </ul>
-        </div>
+        <img src= "/logo-full.png" alt="Logo" className="w-20" />
+        <button class="btn btn-ghost">
+            <BiHome />
+            Home
+        </button>
     </div>
-    <div className="navbar-center">
-        <img src="/logo-full.png" alt="Logo" className="w-20" />
-    </div>
+
     <div className="navbar-end">
-        <button className="btn btn-ghost btn-circle">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /> </svg>
+        <button className="btn">
+            <IoMdSearch type="search" className="grow" placeholder="Search"/>
+        <input type="search" className="grow" placeholder="Search" />
         </button>
         <button className="btn btn-ghost btn-circle">
-        <div className="indicator">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /> </svg>
-            <span className="badge badge-xs badge-primary indicator-item"></span>
-        </div>
+            <FaUser />
         </button>
     </div>
     </div>
