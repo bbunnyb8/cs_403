48c9b4eb684c1435bdaba3a402b4189d4751cfbe
parewaa <eisariya.pw@gmail.com>
2025-05-16 18:58:47 +0700

    edit stock

--- src/app/back_office/stock/edit.jsx
+++ src/app/back_office/stock/edit.jsx
@@ -6,22 +6,9 @@ export default function Edit({ productId, productName, productPrice, productAmou
   const [name, setName] = useState(productName || "");
   const [price, setPrice] = useState(productPrice || "");
   const [amount, setAmount] = useState(productAmount || "");
-  const [image, setImage] = useState(null);
   const [previewUrl, setPreviewUrl] = useState(null);
   
   const modalId = `edit-modal-${productId}`;
-
-  const handleImageChange = (e) => {
-    const file = e.target.files[0];ไไ
-    setImage(file);
-    if (file) {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        setPreviewUrl(reader.result);
-      };
-      reader.readAsDataURL(file);
-    }
-  };
 
 const handleSubmit = async () => {
   console.log("Product ID being submitted:", productId); // ตรวจสอบค่าที่ส่งไป
@@ -67,44 +54,25 @@ const handleSubmit = async () => {
 
 
       <dialog id={modalId} className="modal">
-
         <div className="modal-box max-w max-h flex flex-col">
           <form method="dialog">
             <h2 className="font-bold text-lg mb-2">Edit Product</h2>
 
             {/* Product ID (ถ้าต้องใช้เพิ่มในอนาคต) */}
             {/* Product ID */}
-<div className="flex flex-col p-2">
-  <label className="mb-1 font-medium">
-    Product ID : <span className="font-normal">{productId}</span>
-  </label>
-</div>
-
-            {/* อัปโหลดรูป */}
-            <div className="flex items-center justify-center p-6">
-              <label htmlFor="image-upload" className="cursor-pointer">
-                {previewUrl ? (
-                  <img
-                    src={previewUrl}
-                    alt="Preview"
-                    className="w-40 h-40 object-cover rounded-md"
-                  />
-                ) : (
-                  <RiImageAddLine className="h-40 w-40 text-gray-400" />
-                )}
+            <div className="flex flex-col p-2">
+              <label className="mb-1 font-medium">
+                Product ID : <span className="font-normal">{productId}</span>
               </label>
-              <input
-                id="image-upload"
-                type="file"
-                accept="image/*"
-                onChange={handleImageChange}
-                className="hidden"
-              />
             </div>
 
             {/* Input: name */}
-            <div className="flex p-2">
+            <div className="p-2">
+              <label className="label pb-1">Product Name</label>
+              <label htmlFor={`edit-name-${productName}`}></label>
+              <div className="flex items-center">
               <input
+                id={`edit-name-${productName}`}
                 type="text"
                 className="input validator w-full"
                 placeholder="name"
@@ -112,11 +80,14 @@ const handleSubmit = async () => {
                 onChange={(e) => setName(e.target.value)}
                 required
               />
+              </div>
             </div>
 
             {/* Input: price */}
-            <div className="flex flex-col p-2">
-              <label htmlFor={`edit-price-${productId}`} className="mb-1 font-medium">Price</label>
+            <div className="p-2">
+              <label className="label pb-1">Price</label>
+              <label htmlFor={`edit-price-${productId}`}></label>
+              <div className="flex items-center">
               <input
                 id={`edit-price-${productId}`}
                 type="number"
@@ -128,12 +99,15 @@ const handleSubmit = async () => {
                 value={price}
                 onChange={(e) => setPrice(e.target.value)}
               />
-              <p className="pl-4 text-xs validator-hint text-gray-500">Must enter a number</p>
+              <p className="pl-4 text-xs text-gray-500">Must enter a number</p>
+              </div>
             </div>
 
             {/* Input: amount */}
-            <div className="flex flex-col p-2">
-              <label htmlFor={`edit-amount-${productId}`} className="mb-1 font-medium">Amount</label>
+            <div className="p-2">
+              <label className="label pb-2">Amount</label>
+              <label htmlFor={`edit-amount-${productId}`}></label>
+              <div className="flex items-center">
               <input
                 id={`edit-amount-${productId}`}
                 type="number"
@@ -145,7 +119,8 @@ const handleSubmit = async () => {
                 value={amount}
                 onChange={(e) => setAmount(e.target.value)}
               />
-              <p className="pl-4 text-xs validator-hint text-gray-500">Must enter a number</p>
+              <p className="pl-4 text-xs text-gray-500">Must enter a number</p>
+              </div>
             </div>
           </form>
 
